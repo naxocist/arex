ee40798243b095499562c81f659c185971dd9246
naxocist <subforwork@gmail.com>
2026-04-23 16:07:10 +0700

    fix: security directives

--- next.config.mjs
+++ next.config.mjs
@@ -13,11 +13,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://storage.googleapis.com https://*.tile.openstreetmap.org https://unpkg.com",
-              "connect-src 'self' http://127.0.0.1:54321 http://localhost:8000 https://*.run.app https://nominatim.openstreetmap.org https://storage.googleapis.com",
+              "connect-src 'self' http://127.0.0.1:54321 http://localhost:8000 https://*.run.app https://nominatim.openstreetmap.org https://storage.googleapis.com https://cloudflareinsights.com",
               "frame-ancestors 'none'",
             ].join('; '),
           },
